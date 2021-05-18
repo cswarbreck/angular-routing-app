12d4bb7666e73f924659fe1d7057bd1101d3e0e2
Cosmo Swarbreck <c.swarbreck@gmail.com>
2021-05-18 17:28:19 +0100

    Adds route subscription

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-first',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
-  constructor() { }
+  // public means the html can access it
+  // private means it's not needed in the front end.
+
+
+  // Explicit type decalres the TYPE without a value
+  // public name: String;
+
+  // This is a type of 'any'
+  // public name;
+
+  // This is Implicit decalres name as an empty string.
+  public name = '';
+  public id;
+
+  constructor(
+    // route: has a type of 'ActivatedRoute'
+    // Types are not limited to primitive data types
+    // ActivatedRoutes is an INTERFACE which has defined variables and methods in it
+
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
+    //.subscribe is 'listening' to this.route.queryParams using RxJS
+    // This is an improvement on promises that only fire once
+    this.route.queryParams.subscribe(params => {
+      this.name = params['item1'];
+    })
   }
 
 }
