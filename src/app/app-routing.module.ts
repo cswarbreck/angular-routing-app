37145e08ca98576dd1131b657b29ffcbac9a592e
Cosmo Swarbreck <c.swarbreck@gmail.com>
2021-05-18 20:00:50 +0100

    Adds wildcard route for page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FirstComponent } from './first/first.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SecondComponent } from './second/second.component';
-
 
 const routes: Routes = [
   { path: 'first-component', component: FirstComponent },
-  { path: 'second-component', component: SecondComponent }
+  { path: 'second-component', component: SecondComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
