79cfdb59c83703d711a8f7e46031d19d0b1bc99b
Cosmo Swarbreck <c.swarbreck@gmail.com>
2021-05-25 19:55:05 +0100

    installs webpack

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ChildAComponent } from './child-a/child-a.component';
 import { ChildBComponent } from './child-b/child-b.component';
 import { FirstComponent } from './first/first.component';
+import { HomePageComponent } from './home-page';
+import { LoginPageComponent } from './login-page';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SecondComponent } from './second/second.component';
 
@@ -21,8 +23,11 @@ const routes: Routes = [
     ],
   },
   { path: 'second-component', component: SecondComponent },
-  { path: '', redirectTo: '/first-component', pathMatch: 'full'},
-  { path: '**', component: PageNotFoundComponent }
+  { path: '', component: HomePageComponent },
+  { path: 'login-page', component: LoginPageComponent },
+  { path: '**', redirectTo: '' }
+  // { path: '', redirectTo: '/first-component', pathMatch: 'full'},
+  // { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
